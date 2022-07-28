976651eff23ff7d4d8c3cde32abbe701703c666c
Mathieu D <80090933+mathieudbk@users.noreply.github.com>
2022-07-28 10:45:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 with st.echo(code_location='below'):
     
+    # Format the header.
     st.title('My Parents New Healthy Diner')
     st.header('Breakfast Menu')
              
@@ -25,26 +26,11 @@ with st.echo(code_location='below'):
     st.text('Kale, Spinach & Rocket Smoothie')
     st.text('Hard-Boilded Fre-Range Egg')
     
+    # Get data sample from Snowflake AWS S3 bucket.
+    my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    st.dataframe(my_fruit_list)
     
     
-    """ Part 2 """
     
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    
+   
