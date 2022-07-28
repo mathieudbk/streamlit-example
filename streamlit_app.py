1ad6831b4cff9f23a1d613ef92d4558b8fbb1901
Mathieu D <80090933+mathieudbk@users.noreply.github.com>
2022-07-28 10:50:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ with st.echo(code_location='below'):
     
     # Get data sample from Snowflake AWS S3 bucket.
     my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    my_fruit_list = my_fruit_list.set_index('Fruit')
     
     # Add multi-select picker.
     st.multiselect("Pick some fruits:", list(my_fruit_list.index))
